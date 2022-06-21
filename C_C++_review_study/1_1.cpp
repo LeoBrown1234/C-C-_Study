e3d8343549b00d1cdfdb378676f7c318ad0cd140
Leo_Brown <448996350@qq.com>
2022-06-21 19:30:32 +0800

    basic C/C++ content

---
+++ C_C++_review_study/1_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	float a =2e-2;
+	 
+	cout << "a:" << a << endl;
+	
+
+	system("pause");
+	return 0;
+}
--- C_C++_review_study/test_main.cpp
+++ C_C++_review_study/test_main.cpp
@@ -1,1 +1,38 @@
 //This is my C/C++ learning code.I will store all my learn in this file.
+
+/*
+*标识符规则
+* 不能是关键字
+* 只能是字母、数字、下划线组成
+* 首字符必须是字母/下划线
+* 区分大小写
+* 见名知意
+* 
+* 小驼峰法：firstName
+* 大驼峰法：FirstName
+* 
+* sizeof()统计数据类型所占的内存空间大小
+* float f1=3.14f;
+* 用以表示float型
+* 科学计数法
+* float f2=3e2; //3*10^2
+* float f2=3e-2; //3*0.1^2
+* 
+* 字符型变量256个对应了ASCII码，必须用单引号,如:'a'
+* 且只能包含一个字符
+* (int)a   强制转换a变成int型
+* 
+* 字符串型
+* C语言型:
+* char str1[]="hello world";//双引号
+* 
+* 
+* C++风格型:(要引入string库)
+* string str2="hello world";
+* 
+* 
+* 
+* 
+* 
+* cin>>a;
+*/
